73fde733f7c4d9b3adfc9e4a7de8df24e2c231c8
Sasha Priadchenko <al.pryadchenko@gmail.com>
2023-10-11 11:54:58 +0200

    added functions to added new user before first registration

--- app/server.js
+++ app/server.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("../app");
+const { UserRepositories } = require("../controllers/auth/userRepositories");
+const adminData = require("./adminData.json");
 
 const { PORT = 1594, DB_HOST } = process.env;
 // const { DB_HOST } = process.env;
@@ -8,8 +10,16 @@ const { PORT = 1594, DB_HOST } = process.env;
 
 mongoose
     .connect(DB_HOST)
-    .then(() => {
-        console.log(`${PORT}, connected`);
+    .then(async () => {
+        const users = await UserRepositories.findAllUsers();
+
+        if (users.length === 0) {
+            console.log("No users found, creating admin...");
+            await UserRepositories.createUser(adminData, adminData.password);
+            console.log("Admin user created");
+        }
+
+        console.log(`Server running on port ${PORT}`);
         app.listen(PORT);
     })
     .catch((error) => {
